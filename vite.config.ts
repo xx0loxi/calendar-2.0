5ea16a3d8cb7c48ee3e6e8fe7b115bb16d47d4ed
xx0loxi <radmiradmir225@gmail.com>
2026-02-19 12:00:15 +0200

    Deploy version 3.1.4

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  plugins: [react()],
-  base: command === 'build' ? '/calendar-2.0/' : '/',
-}))
